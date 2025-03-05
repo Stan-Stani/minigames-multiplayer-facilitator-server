ebae2531b6bfd847f56f5c48f1e0ee59b9401f4f
biometricPsychography <cyclinglinguist@gmail.com>
2025-03-04 22:09:42 -0600

    refactor: use built in peer discovery

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ const peerServer = PeerServer({
   host: hostname,
   port: peerServerPort,
   path: '/',
+  allow_discovery: true
 });
 console.log(`Peer Server running at http://${hostname}:${peerServerPort}/`);
 
@@ -17,30 +18,30 @@ peerServer.on("connection", (client) => {
   currentClients.push(client);
 });
 
-const connectedClientIdServerPort = 41362;
+// const connectedClientIdServerPort = 41362;
 
-const server = http.createServer((req, res) => {
-  // Allow localhost with any port during development
-  const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
-  const origin = req.headers.origin ?? "";
+// const server = http.createServer((req, res) => {
+//   // Allow localhost with any port during development
+//   const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
+//   const origin = req.headers.origin ?? "";
 
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
 
-  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
-  res.setHeader("Access-Control-Max-Age", 2592000); // 30 days
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  const currentClientIdsStringified = JSON.stringify({
-    currentClientIds: currentClients.map((c) => c.getId()),
-  });
+//   res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+//   res.setHeader("Access-Control-Max-Age", 2592000); // 30 days
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "application/json");
+//   const currentClientIdsStringified = JSON.stringify({
+//     currentClientIds: currentClients.map((c) => c.getId()),
+//   });
 
-  res.end(currentClientIdsStringified);
-});
+//   res.end(currentClientIdsStringified);
+// });
 
-server.listen(connectedClientIdServerPort, hostname, () => {
-  console.log(
-    `Connected Client Id Server running at http://${hostname}:${connectedClientIdServerPort}/`
-  );
-});
+// server.listen(connectedClientIdServerPort, hostname, () => {
+//   console.log(
+//     `Connected Client Id Server running at http://${hostname}:${connectedClientIdServerPort}/`
+//   );
+// });
