13c39d01821767dd5d9b2bc63443064e7e28f349
biometricPsychography <cyclinglinguist@gmail.com>
2025-03-18 20:43:31 -0500

    wip: add more logging

--- index.ts
+++ index.ts
@@ -6,11 +6,11 @@ const peerServerPort = 41361;
 const peerServer = PeerServer({
   host: hostname,
   port: peerServerPort,
-  path: '/',
+  path: "/",
   allow_discovery: true,
   corsOptions: {
-    origin: "*"
-  }
+    origin: true,
+  },
 });
 console.log(`Peer Server running at http://${hostname}:${peerServerPort}/`);
 
@@ -19,6 +19,18 @@ const currentClients: IClient[] = [];
 peerServer.on("connection", (client) => {
   console.log("New client connection received:", `Id ${client.getId()}`);
   currentClients.push(client);
+});
+
+peerServer.on("error", (error) => {
+  console.log(error);
+});
+
+peerServer.on("message", (client, message) => {
+  console.log(`${client.getId()} says`, message);
+});
+
+peerServer.on("disconnect", (client) => {
+  console.log(`${client.getId()} disconnected`);
 });
 
 // const connectedClientIdServerPort = 41362;
